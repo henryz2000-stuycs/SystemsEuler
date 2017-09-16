839c789763c739219b0846f6e33e144fc29226c4
henryz2000 <hzheng3@stuy.edu>
2017-09-16 01:38:34 -0400

    finished prob2

--- work02.c
+++ work02.c
@@ -4,23 +4,36 @@ void prob1(){
   int sum = 0;
   int three = 3;
   int five = 5;
-  while (five < 1000 || three < 1000){
-    if (three % 5 != 0){
-      sum += 3;
-    }
-    sum += 5;
-    three++;
-    five++;
+  int fifteen = 15;
+  while (three < 1000){
+    sum += three;
+    three += 3;
+  }
+  while (five < 1000){
+    sum += five;
+    five += 5;
+  }
+  while (fifteen < 1000){
+    sum -= fifteen;
+    fifteen += 15;
   }
   printf("%d\n",sum);
 }
 
 void prob2(){
   int first = 1;
-  int next = 2;
-  while (next < 4000000){
-
+  int second = 2;
+  int next = 3;
+  int sum = 0;
+  while (second < 4000000){
+    if (second % 2 == 0){
+      sum += second;
+    }
+    first = second;
+    second = next;
+    next = first + second;
   }
+  printf("%d\n",sum);
 }
 
 int main(){
